a2c21899b3b34d535b5410f4c02f1c2f59ac754d
Luter <lutersergei@mail.ru>
2016-06-02 01:52:24 +0700

    поменял проверку на тип файла и ошибки

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ session_start();
 error_reporting(E_ALL);
 define('MAX_SIZE', 5000000);
 define('DANGER_NOT_IMAGE','Файл не является изображением');
+define('ERROR','Произошла ошибка');
 define('DANGER_SIZE_EXCEEDED','Превышен максимальный размер файла');
 define('SUCCESS','Изображение успешно загружено');
 $folder=false;
 $result=false;
-include 'func_make_thumb.php';
-//Передача результата из сессии в переменную
+//Явно не лучший скрипт для создания превью. Но работает
+include 'func_make_thumb.php';          
+//Передача результата из сессии в переменную 
 if (isset($_SESSION['result']))
 {
     $result=$_SESSION['result'];
@@ -25,13 +27,21 @@ if (!is_dir($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'img'))
 else $folder=opendir($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'img');
 if ((isset($_FILES['images'])))
 {           //Проверка типа загружаемого файла
-    if (strpos($_FILES['images']['type'],"image")!==false)
+    $type_array=getimagesize($_FILES['images']['tmp_name']);
+    $_SESSION['type']=$type_array[2];
+    if (($type_array[2])===(1)||($type_array[2])===(2)||($type_array[2])===(3))
         {   //Проверка размера файла
             if (($_FILES['images']['size'])<MAX_SIZE)
             {
-                move_uploaded_file($_FILES['images']['tmp_name'],$_SERVER['DOCUMENT_ROOT'].'/'.'img/'.$_FILES['images']['name']);
-                makeThumbnails('img_thumbnails/','img/'.$_FILES['images']['name'],$_FILES['images']['name']);
-                $_SESSION['result']=SUCCESS;
+                if (($_FILES['images']['error'])===0)   //На проверку всех ошибок не хватило времени
+                {
+                    move_uploaded_file($_FILES['images']['tmp_name'],$_SERVER['DOCUMENT_ROOT'].'/'.'img/'.$_FILES['images']['name']);
+                    makeThumbnails($_SERVER['DOCUMENT_ROOT'].'/'.'img_thumbnails/',$_SERVER['DOCUMENT_ROOT'].'/'.'img/'.$_FILES['images']['name'],$_FILES['images']['name']);
+                    $_SESSION['result']=SUCCESS;
+                    header("Location: index.php");
+                    die();
+                }
+                else $_SESSION['result']=ERROR;
                 header("Location: index.php");
                 die();
             }
@@ -74,13 +84,13 @@ if ((isset($_FILES['images'])))
             ?>
         </div>
         <div class="col-md-6">
-            <h1 class="title">Gallery (Pre-Alpha Version)</h1>
+            <h1 class="title">Image Gallery (Pre-Alpha Version)</h1>
             <?php
             if (isset($files_array))
             {
                 foreach ($files_array as $files)
                 {
-                    echo "<div class=\"col-xs-4 col-md-3\">
+                    echo "<div class=\"col-md-3\">
                           <a class=\"thumbnail\" href=\"img/{$files}\" target=\"_blank\"><img src=\"img_thumbnails/{$files}\"></a>
                       </div>";
                 }
